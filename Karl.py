328a83dd07b57cbebf1ad4c75fdfdaef5f557094
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2022-11-29 20:28:23 -0600

    Update Karl.py

--- Karl.py
+++ Karl.py
@@ -5,12 +5,37 @@ import json
 try: from types import SimpleNamespace as Namespace
 except ImportError: from argparse import Namespace
 import httplib2
+args = sys.argv
 url = json.loads(open('./url.json').read(), object_hook=lambda d: Namespace(**d))
 def send(_in, url): print(httplib2.Http().request(uri=url, method='POST', headers={'Content-Type': 'application/json; charset=UTF-8'},body=json.dumps(_in)))
 def beemovie(_in): with open('beemovie.txt') as f: for x in f.read().splitlines(): send(x,who)
-def main(args):
+def hilfe():
+  t = f"""
+Usage: cmd [OPTION] [idk]
+this isnt done yet idk
+
+    -h, --help             give this help list
+    -g, --get              .
+    -t, --text             send message to given user
+
+"""
+  print(t)
+def usage(arg):
+  print(f"Error:   {a}  is not a valid entry")
+  hilfe()
+
+def sw_1(a):
+  sw = {
+    "-h": hilfe(),
+    "--help": hilfe(),
+    "-g": get(),
+    "--get": get()
+  }
+  return sw.get(a, usage(a))
+
+def main():
   if args[1:]:
-    
+    sw_1(args[1])
   else: usage()
 
-if __name__ == "__main__": main(sys.argv)
+if __name__ == "__main__": main()
